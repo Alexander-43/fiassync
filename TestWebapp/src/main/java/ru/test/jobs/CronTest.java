dc170d70f4193513e9bb2ae308b5b8442c2ab336
admin <admin@10.0.2.15>
2015-10-06 00:14:49 +0300

    add projects for alfresco & share

---
+++ TestWebapp/src/main/java/ru/test/jobs/CronTest.java
@@ -0,0 +1,36 @@
+package ru.test.jobs;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.quartz.CronExpression;
+import java.text.ParseException;
+
+public class CronTest {
+	
+	public static void main(String[] args) {
+		Date date;
+	    CronExpression exp;
+	    // Run every 10 minutes and 30 seconds in the year 2002
+	    String a = "30 */10 * * * ? 2002";      
+	    // Run every 10 minutes and 30 seconds of any year
+	    String b = "0 0,30 12,13,14 * * ? *"; 
+	    int i = 0;
+	    Map<Date, Integer> map = new HashMap<>(); 
+	    try {
+	        date = new Date();
+	        exp = new CronExpression(b);
+	        
+	    	while (date != null){
+		        date = exp.getNextValidTimeAfter(date);
+		        System.out.println(date.getTime());       // Tue Nov 04 19:20:30 PST 2014
+		        ++i;
+		        map.put(date, new Integer(i));
+	    	}
+	    } catch (ParseException e) {
+	        e.printStackTrace();
+	    }
+	    System.out.println(i);
+	}
+}
